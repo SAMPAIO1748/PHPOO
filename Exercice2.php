012f32185d43ac135a517376936abbe0e1dab2e5
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-02-01 15:22:04 +0100

    Exercice 2 corrigé

--- Exercice2.php
+++ Exercice2.php
@@ -1,16 +1,14 @@
 <?php
 
-class Vehicule
+abstract class Vehicule // classe véhicule non instanciable car abstraite
 {
-    public function demarrer()
+    final public function demarrer()
     {
         return 'je demarre';
     }
 
-    public function carburant()
-    {
-        return;
-    }
+    abstract public function carburant();
+
 
     public function nombreTestObligatoire()
     {
@@ -18,17 +16,46 @@ class Vehicule
     }
 }
 
-class Peugeot
+class Peugeot extends Vehicule
 {
+    public function carburant()
+    {
+        return 'essence';
+    }
+
+    public function nombreTestObligatoire()
+    {
+        return parent::nombreTestObligatoire() + 70;
+    }
 }
 
-class Renault
+class Renault extends Vehicule
 {
-}
+    public function carburant()
+    {
+        return 'diesel';
+    }
 
+    public function nombreTestObligatoire()
+    {
+        return parent::nombreTestObligatoire() + 30;
+    }
+}
 // 1. Faire en sorte de ne pas avoir d'objet Vehicule.
 // 2. Obligation pour la Renault et la Peugeot de posséder la même méthode demarrer() qu'un véhicule de base
 // 3. Obligation pour la Renault de déclarer un carburant diesel et le Peugeot un carburant essence.
 // 4. La Renault doit effectuer 30 tests de plus qu'un véhicule de base.
 // 5. La Peugeot doit effectuer 70 tests de plus qu'un véhicule de base.
-// 6. Effectuer les affichages necessaires. 
+// 6. Effectuer les affichages necessaires.
+
+$renault = new Renault;
+echo "Renault " . $renault->demarrer() . '<br>';
+echo "Renault " . $renault->carburant() . '<br>';
+echo "Renault " . $renault->nombreTestObligatoire() . ' tests <br>';
+
+echo '<hr>';
+
+$peugeot = new Peugeot;
+echo "Peugeot " . $peugeot->demarrer() . '<br>';
+echo "Peugeot " . $peugeot->carburant() . '<br>';
+echo "Peugeot " . $peugeot->nombreTestObligatoire() . ' tests <br>';
