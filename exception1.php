a274393173161500101452ea7b851782a80c191c
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-02-02 14:14:30 +0100

    exception 2

--- exception1.php
+++ exception1.php
@@ -21,11 +21,22 @@ $listes = array("Apple", "Samsung", "LG", "Sony");
 
 try {
     // try : les essais sur la finction recherche
-    echo recherche($listes, 'Apple');
-    echo recherche($listes, 'Samsung');
-    echo recherche($tab, 'Roger');
+    echo recherche($listes, 'Apple') . "<br>";
+    echo recherche($listes, 'Samsung') . "<br>";
+    //echo recherche($liste, 'Bruce Wayne');
+    echo recherche($tab, 'Roger') . "<br>";
+    //echo 'toto';
 } catch (Exception $exception) {
     // bloc de capture, on va capturer les exceptions comme Throw est utilisé avec new Execption, 
     // le traitement du code est stoppé et l'erreur est enregistré dans la variable $exception.
+    // On peut mettre plusieurs catch d'affilé et try et catch sont toujours ensemble
     echo 'Erreur : ' . $exception->getMessage() . '<br>';
+    var_dump($exception);
 }
+
+/**
+ * - Exception est une classe prédéfinie. 
+ * - Une exception est une erreur que l'on peut attraper grâce aux mots-clé try et catch.
+ * - Throw permet d'arreter l'execution du code écrit dans le try et de rentrer dans le catch.
+ * - Try et Catch permettent d'avoir 2 blocs d'instructions distinct.
+ * */
