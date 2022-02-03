01f9173213c6681ca768b19769afc8de6dd53acf
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-02-03 12:16:39 +0100

    Create creation

---
+++ PDO/ajout_creation.php
@@ -0,0 +1,26 @@
+<?php
+
+$titre = $_POST['titre'];
+$description = $_POST['description'];
+$galerie = $_POST['galerie'];
+$date = date('Y-m-d');
+
+$bdd = new PDO('mysql:host=localhost;dbname=bibliotheque', 'root', 'root');
+// Connexion à la base de données
+$sql = "INSERT INTO creation (titre, description, galerie, date_creation) VALUES (:titre, :description, :galerie, :date)";
+// Requête SQL sous forme de chaîne de caractères PHP
+$requete = $bdd->prepare($sql);
+// Préparation de la requete
+$requete->bindValue(':titre', $titre, PDO::PARAM_STR);
+// Attribution de la valeur à la variable
+$requete->bindValue(':description', $description, PDO::PARAM_STR);
+// Attribution de la valeur à la variable
+$requete->bindValue(':galerie', $galerie, PDO::PARAM_STR);
+// Attribution de la valeur à la variable
+$requete->bindValue(':date', $date, PDO::PARAM_STR);
+// Attribution de la valeur à la variable
+$requete->execute();
+// Execution de la requête
+
+header('Location: list_creation.php');
+// Redirection vers la page list_creation.php
---
+++ PDO/ajout_form_creation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Ajouter une création</h1>
+    <form action="ajout_creation.php" method="post">
+
+        <input type="text" name="titre" placeholder="Le nom">
+        <input type="text" name="description" placeholder="la description">
+        <input type="text" name="galerie" placeholder="la gallerie">
+        <input type="submit" value="Enregistrer">
+
+    </form>
+
+</body>
+
+</html>
